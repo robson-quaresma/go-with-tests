2f0a0300115ec9edfbc2567424fc811924a13c18
Robson Quaresma <robquaresma@gmail.com>
2024-04-14 13:25:02 -0300

    fail

--- hello_test.go
+++ hello_test.go
@@ -22,6 +22,12 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jean", "French")
+		want := "Bonjour, Jean"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
