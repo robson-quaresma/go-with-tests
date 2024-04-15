f5e65940c1ec68ae59076cc29d3d1c28a90f4c9e
Robson Quaresma <robquaresma@gmail.com>
2024-04-15 11:27:22 -0300

    write the  SumAllTails - arrays and slices

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -34,6 +34,15 @@ func TestSumAll(t *testing.T) {
 	want := []int{3, 9}
 
 	if !slices.Equal(got, want) {
-		t.Errorf("got '%d' want %v", got, want)
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got '%v' want '%v'", got, want)
 	}
 }
