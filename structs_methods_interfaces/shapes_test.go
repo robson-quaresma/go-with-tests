4cf4e5f3d371817e09cd144f0a44d0d9df5e5781
Robson Quaresma <robquaresma@gmail.com>
2024-04-15 13:13:23 -0300

    create a circle area test - structs, methods and interfaces

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -13,11 +13,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectagle := Rectagle{12.0, 6.0}
-	got := Area(rectagle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got '%.2f' want '%.2f'", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectagle := Rectagle{12.0, 6.0}
+		got := Area(rectagle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got '%.2f' want '%.2f'", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got '%g' want '%g'", got, want)
+		}
+	})
+
 }
