bef412981fa75682cb93ed5ca5df5b3cc3878f7a
Robson Quaresma <robquaresma@gmail.com>
2024-04-15 10:26:08 -0300

    change to slice instead of arrays - arrays and slices

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -3,16 +3,25 @@ package arraysslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	// explict array declaration
-	// numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	// implicit array declaration
-	numbers := [...]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	got := Sum(numbers)
-	want := 15
+		if got != want {
+			t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
-	}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
+		}
+	})
 }
