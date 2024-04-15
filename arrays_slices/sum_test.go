7c049f2fcf6125c343192a24b1556c1c809ed4da
Robson Quaresma <robquaresma@gmail.com>
2024-04-15 11:09:08 -0300

    create new Test SumAll - arrays and slices

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -25,3 +25,12 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAll(t *testing.B) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if got != want {
+		t.Errorf("got '%d' want %v", got, want)
+	}
+}
