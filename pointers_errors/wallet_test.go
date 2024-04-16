8fcfb99dfa28ee448028e9fef59063b5364b90f9
Robson Quaresma <robquaresma@gmail.com>
2024-04-16 10:14:26 -0300

    refactoring error in test add t.Fatal - pointers and erros

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -15,10 +15,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t testing.TB, err error) {
+	assertError := func(t testing.TB, got error, want string) {
 		t.Helper()
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got '%q' want '%q'", got, want)
 		}
 	}
 
@@ -39,7 +43,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		assertError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 		assertBalance(t, wallet, startBalance)
 	})
 }
