82beefce7cd7ac750f030ba2fc72ec4545990c8d
Robson Quaresma <robquaresma@gmail.com>
2024-04-16 17:32:46 -0300

    mocking: adjust our main function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -19,11 +19,17 @@ type SpySleeper struct {
 	Calls int
 }
 
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
