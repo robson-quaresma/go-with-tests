eded8d7a8593cf04dd5b3358aa7792f21e39d5ed
Robson Quaresma <robquaresma@gmail.com>
2024-04-16 11:36:18 -0300

    refactor - maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,5 +1,20 @@
 package maps
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got '%q' want '%q'", got, want)
+	}
 }
