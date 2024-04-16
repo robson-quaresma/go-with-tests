74df445e1835b98915f06d6e7cf6be384bfcbcaf
Robson Quaresma <robquaresma@gmail.com>
2024-04-16 17:13:09 -0300

    mocking: create test

---
+++ dependency_injection/main.go
@@ -0,0 +1,10 @@
+package dependencyinjection
+
+// import (
+// 	"log"
+// 	"net/http"
+// )
+
+// func main() {
+// 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+// }
