8fc2bab0236eff9b268784597ce2f1d7cf10ac27
Robson Quaresma <robquaresma@gmail.com>
2024-04-16 17:24:50 -0300

    mocking: add sleep

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const (
@@ -14,6 +15,7 @@ func Countdown(out io.Writer) {
 
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Fprint(out, finalWord)
