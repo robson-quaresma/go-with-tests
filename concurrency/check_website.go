d07ff1515cfd956c286de27a99205c9b5a55feeb
Robson Quaresma <robquaresma@gmail.com>
2024-04-17 12:54:51 -0300

    concurrency: add sleep for pass the test

--- concurrency/check_website.go
+++ concurrency/check_website.go
@@ -1,5 +1,7 @@
 package concurrency
 
+import "time"
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
@@ -11,6 +13,6 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 			results[url] = wc(url)
 		}()
 	}
-
+	time.Sleep(2 * time.Second)
 	return results
 }
