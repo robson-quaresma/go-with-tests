dfb931853edf30a0f31c48b84b017c5d8882c0b7
Robson Quaresma <robquaresma@gmail.com>
2024-04-17 16:37:34 -0300

    select: using select

--- select/racer.go
+++ select/racer.go
@@ -2,21 +2,35 @@ package selectp
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
 
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+/*
+*
+In our case, we don't care what type is sent to the channel,
+we just want to signal we are done and closing the
+channel works perfectly!
+
+Why struct{} and not another type like a bool? Well,
+a chan struct{} is the smallest data type available
+from a memory perspective so we get no allocation versus a bool.
+Since we are closing and not sending anything on the chan,
+why allocate anything?
+*/
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
