38f3fa18ad2765ee96640c747294be9b720643c9
Robson Quaresma <robquaresma@gmail.com>
2024-04-17 12:16:47 -0300

    concurrency: setup

---
+++ mocking/main.go
@@ -0,0 +1,12 @@
+package mocking
+
+// import (
+// 	"os"
+
+// 	"github.com/robson-quaresma/go-with-tests/mocking"
+// )
+
+// func main() {
+// 	sleeper := &mocking.DefaultSleeper{}
+// 	mocking.Countdown(os.Stdout, sleeper)
+// }
