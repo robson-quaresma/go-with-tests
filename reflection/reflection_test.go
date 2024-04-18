725b4b3af778811d16d7dabad469e0f5397da64f
Robson Quaresma <robquaresma@gmail.com>
2024-04-18 13:47:25 -0300

    reflection: add a new scenario

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -27,6 +27,14 @@ func TestWalk(t *testing.T) {
 			}{"Chris", "London"},
 			[]string{"Chris", "London"},
 		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Cris"},
+		},
 	}
 
 	for _, test := range cases {
