eee24319ba96f5cd0043954a02f4607720c61f9a
Robson Quaresma <robquaresma@gmail.com>
2024-04-20 11:28:47 -0300

    sync: pass the test

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,14 @@
 package sync
 
-type Counter struct{}
+type Counter struct {
+	value int
+}
 
 // methods belongs to struct
-func (c *Counter) Inc() {}
+func (c *Counter) Inc() {
+	c.value++
+}
 
 func (c *Counter) Value() int {
-	return 0
+	return c.value
 }
